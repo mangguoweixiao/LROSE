2ea1ecda11a4a267dc11eb4031f863d7a0066521
hewenjuan <2545625776@qq.com>
2017-04-27 11:16:13 +0800

    new project new

---
+++ EFCodeFirstDemo/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LROSE_BLL;
+using LROSE_BLL.MR;
+using LROSE_Model;
+using LROSE_Model.MrData;
+using LROSE_DAL;
+using System.Data.Entity;
+using System.Xml;
+using System.Configuration;
+
+
+namespace EFCodeFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string asd = "Data Source=I1HLPK93QO6EFE9;Initial Catalog=asdqwer;Integrated Security=True";;
+            string asd1 = "asdads";
+            using (var db = new LROSRDbContext())
+            {
+                db.Configuration.AutoDetectChangesEnabled = false;
+                db.Configuration.ValidateOnSaveEnabled = false;
+
+                MrTableAllColumn mr = new MrTableAllColumn ();
+                mr.KPid =1;
+                db.MrTableAllColumn.Add(mr);
+                db.SaveChanges();
+                int du=db.MrTableAllColumn.Count();
+
+            }
+
+
+
+            //using (var db = new LROSRDbContext())
+            //{
+            //    var a = db.MrTableAllColumn.FirstOrDefault();
+            //    int b = db.MrTableAllColumn.Count();
+            //}
+
+            //string AppKey = "name";
+            //string AppValue = "this";
+            //try
+            //{
+            //    XmlDocument xDoc = new XmlDocument();
+            //    //获取App.config文件绝对路径
+            //    String basePath = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            //    basePath = basePath.Substring(0, basePath.Length - 10);
+            //    String path = basePath + "App.config";
+            //    xDoc.Load(path);
+
+            //    XmlNode xNode;
+            //    XmlElement xElem1;
+            //    XmlElement xElem2;
+            //    //修改完文件内容，还需要修改缓存里面的配置内容，使得刚修改完即可用
+            //    //如果不修改缓存，需要等到关闭程序，在启动，才可使用修改后的配置信息
+            //    Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //    xNode = xDoc.SelectSingleNode("configuration/connectionStrings");
+            //    xElem1 = (XmlElement)xNode.SelectSingleNode("add[@name='" + AppKey + "']");
+            //    if (xElem1 != null)
+            //    {
+            //        xElem1.SetAttribute("connectionString", AppValue);
+            //        cfa.AppSettings.Settings["AppKey"].Value = AppValue;
+            //    }
+            //    else
+            //    {
+            //        xElem2 = xDoc.CreateElement("add");
+            //        xElem2.SetAttribute("name", AppKey);
+            //        xElem2.SetAttribute("connectionString", AppValue);
+            //        xNode.AppendChild(xElem2);
+            //        cfa.AppSettings.Settings.Add(AppKey, AppValue);
+            //    }
+            //    //改变缓存中的配置文件信息（读取出来才会是最新的配置）
+            //    cfa.Save();
+            //    ConfigurationManager.RefreshSection("configuration/connectionStrings");
+
+            //    xDoc.Save(path);
+
+            //    //Properties.Settings.Default.Reload();
+            //}
+            //catch (Exception e)
+            //{
+            //    string error = e.Message;
+
+            //}
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ LROSE_Mode/MrData/PSRP.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using LROSE_Model.MrData;
+
+namespace LROSE_Model.MrData
+{
+    //所有需要在数据库中建的表
+
+    /// <summary>
+    /// mrs RSRP 表
+    /// </summary>
+    public class RSRP : MrTableBasic 
+    {
+        [Key]
+        public string KPid { get; set; }
+        public string RSRP_00 { get; set; }
+        public string RSRP_01 { get; set; }
+        public string RSRP_02 { get; set; }
+        public string RSRP_03 { get; set; }
+        public string RSRP_04 { get; set; }
+        public string RSRP_05 { get; set; }
+        public string RSRP_06 { get; set; }
+        public string RSRP_07 { get; set; }
+        public string RSRP_09 { get; set; }
+        public string RSRP_10 { get; set; }
+        public string RSRP_11 { get; set; }
+        public string RSRP_12 { get; set; }
+        public string RSRP_13 { get; set; }
+        public string RSRP_14 { get; set; }
+        public string RSRP_15 { get; set; }
+        public string RSRP_16 { get; set; }
+        public string RSRP_17 { get; set; }
+        public string RSRP_19 { get; set; }
+        public string RSRP_20 { get; set; }
+        public string RSRP_21 { get; set; }
+        public string RSRP_22 { get; set; }
+        public string RSRP_23 { get; set; }
+        public string RSRP_24 { get; set; }
+        public string RSRP_25 { get; set; }
+        public string RSRP_26 { get; set; }
+        public string RSRP_27 { get; set; }
+        public string RSRP_28 { get; set; }
+        public string RSRP_29 { get; set; }
+        public string RSRP_30 { get; set; }
+        public string RSRP_31 { get; set; }
+        public string RSRP_32 { get; set; }
+        public string RSRP_33 { get; set; }
+        public string RSRP_34 { get; set; }
+        public string RSRP_35 { get; set; }
+        public string RSRP_36 { get; set; }
+        public string RSRP_37 { get; set; }
+        public string RSRP_38 { get; set; }
+        public string RSRP_39 { get; set; }
+        public string RSRP_40 { get; set; }
+        public string RSRP_41 { get; set; }
+        public string RSRP_42 { get; set; }
+        public string RSRP_43 { get; set; }
+        public string RSRP_44 { get; set; }
+        public string RSRP_45 { get; set; }
+        public string RSRP_46 { get; set; }
+        public string RSRP_47 { get; set; }
+    } 
+}
