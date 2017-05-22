977d83ab72e0028f0ee1458093d57fb54e49f8ff
du <770573304@qq.com>
2017-05-22 14:33:01 +0800

    PM逻辑修改

---
+++ LROSE_Mode/PMData/PMAllMd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LROSE_Model.PMData
+{
+    public class PMAllMd
+    {
+        public int KPid { get; set; }
+        public string ffv { get; set; }
+        //public string sn_nedn { get; set; }
+        //这是第一个SubNetwork
+        public string SubNetwork { get; set; }
+        //这是第二个SubNetwork
+        public string SubNetwork1 { get; set; }
+        public string MeContext { get; set; }
+        public string cbt { get; set; }
+        public string mts { get; set; }
+        public string nesw { get; set; }
+        public string gp { get; set; }
+        public string neun { get; set; }
+
+        public string moidKey { get; set; }//mo指标
+        public string moidValue { get; set; }//mo值
+        public string mtList { get; set; } //指标名称
+        public string rList { get; set; }//指标值
+        public string moid { get; set; }
+    }
+}
--- LROSE_Mode/PMData/PMAllMoid.cs
+++ LROSE_Mode/PMData/PMAllMoid.cs
@@ -6,16 +6,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LROSE_Model.PMData
 {
-    public class PMALLData
+    /// <summary>
+    /// moid 的关键信息
+    /// </summary>
+    public class PMAllMoid
     {
         [Key]
         public int KPid { get; set; }
 
         public string moidKey { get; set; }//mo指标
         public string moidValue { get; set; }//mo值
+        public string moid { get; set; }
         public string mtList { get; set; } //指标名称
         public string rList { get; set; }//指标值
         public string MeContext { get; set; }//基站名称
+        public string cbt { get; set; }//开始采集数据的时间
     }
 }
 
